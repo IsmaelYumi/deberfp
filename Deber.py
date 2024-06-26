22f92ba8389626f9def391b3349ee791576acdc3
Ismael <ismeyumi@espol.edu.ec>
2024-06-25 23:02:28 -0500

    actualizacion

--- Deber.py
+++ Deber.py
@@ -1,4 +1,5 @@
 lUserPass = ["jmoran-Jm45e", "mflores-blacK2009", "rrodriguez-1234abc", "eperez-paS$word", "lgonzalez-letmein"]
+#Literal B
 def Menuprincipal():
   validar =True
   while(validar==True): 
@@ -13,17 +14,48 @@ def Menuprincipal():
    else:
      validar =False
      return opcion
-
+def Cambio_clave(contra, ListaU):
+  validacion=False
+  validacion2=False
+  validacion3=False
+  clave=input("Ingrese su actual clave")
+  if(clave==contra):
+    print("Para su nueva clave debe seguir los siguientes parametros:")
+    print("1..Debe tener al menos una letra")
+    print("2..Debe tener al menos un numero")
+    print("3..Debee tener al menos 5 carcateres")
+    nclave=input("Ingrese su nueva clave: ")
+    for carcater in nclave:
+      if(carcater.isupper()):
+        validacion=True
+    if(validacion==True):
+      for carcater in nclave:
+        if(carcater.isdigit()):
+          validacion2=True
+    else:
+      print("Error en la clave")
+    if validacion2==True:
+      if(len(nclave)>=5):
+        validacion3==True
+    if(validacion==True and validacion2==True and validacion3==True):
+      for index in range(len(ListaU)):
+        usuario,con=ListaU[index].split("-")
+        if(con==contra):
+          ListaU[index]= usuario + "-" + nclave
+          print( "clave cambiada con exito")
+          return True
 intentos=0
+validar=True
+#Literal A
 while(intentos<3):
   usuario = input("Ingrese su usuario: ")
   contrasena = input("Ingrese su contraseña: ")
   if usuario + "-" + contrasena in lUserPass:
     continuar=input("Presiona el enter para continuar")
-    opcion=Menuprincipal()
-    
-
-    break
+    while(True):
+      opcion=Menuprincipal()
+      if(opcion=="C"):
+        Cambio_clave(contrasena,lUserPass)
   else:
     print("Usuario incorrecto")
     intentos +=1
